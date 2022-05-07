4e269c6bf024107c990c83aaf768f06dc7a30eb7
Vikas Kumar Gupta <vgupta395@gmail.com>
2022-05-08 01:20:15 +0530

    modified sum.java

--- src/Sum.java
+++ src/Sum.java
@@ -3,6 +3,8 @@ public class Sum {
 
 	public static void main(String args[]) {
 		
-		System.out.println("10 + 20 = "+(10+20));
+		int a = 10;
+		int b = 20;
+		System.out.println("a + b = "+(a+b));
 	}
 }
